e133db03574b47749caf01974e29dc4cdaabaec4
liebercanis <mgold@unm.edu>
2022-07-12 15:27:24 -0600

    fix path

--- setupLinux.sh
+++ setupLinux.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 # HDF5: C++ interface
 export HDF5CPP=/usr/lib64
+export HDF5ROOT=$HOME/legend/SPMSRoot
 export LD_LIBRARY_PATH=$HDF5CPP/lib:$LD_LIBRARY_PATH
 # HDF2ROOT environment:
-export HDF5ROOT=$HOME/legend/HDF5root
-export LD_LIBRARY_PATH=$HDF5ROOT/obj:$HDF5ROOT/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HDF5ROOT/obj:$LD_LIBRARY_PATH
 #
 printenv  HDF5ROOT
 printenv  LD_LIBRARY_PATH
